7a6ecd5607474ff677bd03fcd567b312b3442211
Cara Ponzini <ponzini@gmail.com>
2019-03-18 13:33:18 -0700

    created load function and test data rendering

--- src/list-component.js
+++ src/list-component.js
@@ -1,9 +1,15 @@
 export function makeCardTemplate(campsite) {
+    let campsiteURL = null;
+    if(!campsite.MEDIA[0]) {
+        campsiteURL = 'http://noodleblvd.com/wp-content/uploads/2016/10/No-Image-Available.jpg';
+    } else {
+        campsiteURL = campsite.MEDIA[0].URL;
+    }
     const html = /*html*/ `
     <li>
         <a href="./campsite-detail.html" alt="campsite detail page">
             <h3>${campsite.FacilityName}</h3>
-            <img src="${campsite.MEDIA[0].URL}" alt="title.name" class="campsite-image">
+            <img src="${campsiteURL}" alt="title.name" class="campsite-image">
             <p>Location</p>
         </a>
     </li>
