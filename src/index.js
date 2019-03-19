63c6ee6157db3d6a4b757ea26ee7671463e253d7
Cara Ponzini <ponzini@gmail.com>
2019-03-19 10:18:45 -0700

    added scrolling to address volume of results, added styling

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import updateSearchTerm from '../search/search-component.js';
 import { readSearchFromQuery, writeSearchToQuery, writePageToQuery } from '../src/hash-query.js';
 import makeSearchComponent from '../src/make-search-url.js';
 
-// let url = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities?limit=50&full=true&activity=CAMPING&apikey=cb99ea00-0bd2-4742-bd89-341cf682661d&query=eastern+oregon&state=OR&offset=0';
-
 window.addEventListener('hashchange', loadQuery);
 
 function loadQuery() {
-    console.log('hash');
     const query = window.location.hash.slice(1);
     const queryOptions = readSearchFromQuery(query);
     updateSearchTerm(queryOptions.query);
@@ -25,3 +22,4 @@ function loadQuery() {
             loadCard(results.RECDATA);
         });
 }
+
---
+++ src/paging-component.js
@@ -0,0 +1,15 @@
+const previousButton = document.getElementById('previous-button');
+const nextButton = document.getElementById('next-button');
+const currentPage = document.getElementById('current-page');
+const totalPages = document.getElementById('total-pages');
+
+let currentPageNumber = 1;
+
+previousButton.addEventListener() {
+    currentPageNumber++;
+
+}
+
+nextButton.addEventListener() {
+    currentPageNumber--;
+}
