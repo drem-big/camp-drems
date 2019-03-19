4005bd4541fe44c94a265b9329910656177a592e
miniponz <ponzini@gmail.com>
2019-03-19 15:45:20 -0700

    detail and images load from fetch on campsite-detail page

--- src/campsite-detail.js
+++ src/campsite-detail.js
@@ -1,6 +1,5 @@
 import { loadHeader } from './header-component.js';
 import makeDetailTemplate, { makeImageTemplate } from './detail-component.js';
-import data from '../data/single-campsite.js';
 
 const user = {
     displayName: 'Anna',
@@ -8,12 +7,9 @@ const user = {
 };
 loadHeader(user);
 
-
 const facilityUrl = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/251914/?apikey=cb99ea00-0bd2-4742-bd89-341cf682661d';
 const mediaUrl = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/251914/media?limit=50&offset=0&apikey=a21c6558-6a4d-4fc0-a5f0-5f66982ce7e2';
-// if(!url) {
-//     return;
-// }
+
 fetch(facilityUrl)
     .then(res => res.json())
     .then(results => {
@@ -30,13 +26,12 @@ function loadDetail(data) {
 fetch(mediaUrl)
     .then(res => res.json())
     .then(mediaResults => {
-        console.log(mediaResults);
         loadImages(mediaResults);
     })
 
 function loadImages(data) {
-    console.log('load');
     const main = document.getElementById('main');
-    const dom = makeDetailTemplate(data);
+    const dom = makeImageTemplate(data);
     main.appendChild(dom);
 }
+
