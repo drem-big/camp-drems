febfeb7b5d7306e4efc961eba1cb28b194aacb5a
miniponz <ponzini@gmail.com>
2019-03-19 14:50:53 -0700

    facility name and description render from api

--- src/campsite-detail.js
+++ src/campsite-detail.js
@@ -1,5 +1,5 @@
 import { loadHeader } from './header-component.js';
-import makeDetailTemplate from './detail-component.js';
+import makeDetailTemplate, { makeImageTemplate } from './detail-component.js';
 import data from '../data/single-campsite.js';
 
 const user = {
@@ -9,12 +9,12 @@ const user = {
 loadHeader(user);
 // loadDetail(data);
 
-const url = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/251914/?apikey=cb99ea00-0bd2-4742-bd89-341cf682661d';
-
+const facilityUrl = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/251914/?apikey=cb99ea00-0bd2-4742-bd89-341cf682661d';
+const mediaUrl = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/251914/media?limit=50&offset=0&apikey=a21c6558-6a4d-4fc0-a5f0-5f66982ce7e2';
 // if(!url) {
 //     return;
 // }
-fetch(url)
+fetch(facilityUrl)
     .then(res => res.json())
     .then(results => {
         console.log(results);
@@ -24,6 +24,18 @@ fetch(url)
 function loadDetail(data) {
     const main = document.getElementById('main');
     const dom = makeDetailTemplate(data);
-    
+    main.appendChild(dom);
+}
+
+fetch(mediaUrl)
+    .then(res => res.json())
+    .then(results => {
+        console.log(results);
+        loadImages(results);
+    })
+
+function loadImages(data) {
+    const main = document.getElementById('main');
+    const dom = makeDetailTemplate(data);
     main.appendChild(dom);
 }
--- src/detail-component.js
+++ src/detail-component.js
@@ -1,29 +1,27 @@
 export default function makeDetailTemplate(data) {
-    const regex = /\n/gi;
-    const recData = data.FacilityDescription;
-    const newDescription = recData.replace(regex, '');
     const html = /*html*/ `
     <article>
         <h1 id="facility-name">${data.FacilityName}</h1>
-        <h3 id="location">${data.FACILITYADDRESS[0].City}, ${data.FACILITYADDRESS[0].AddressStateCode} </h3>
-        <div>
-        ${data.MEDIA.map(image => {
-            return /*html*/ `
-            <img src="${image.URL}" alt="campsite photo">
+        <p id="location">${data.FacilityDirections}</p>
+        <div>${data.FacilityDescription}</div>
+            </article>
             `;
-        }).join('')}
-        </div>
 
-        <div>${newDescription}</div>
-        <h2>Activities</h2>
-        <ul id="activities-list">
-            ${data.ACTIVITY.map(activity => {
-                return /*html*/`
-                <li>${activity.FacilityActivityDescription}</li>
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+export function makeImageTemplate(data) {
+
+    const html = /*html*/ `
+        <div>
+            ${data.RECDATA.map(image => {
+                return /*html*/ `
+                <img src="${image.URL}" alt="campsite photo">
                 `;
             }).join('')}
-        </ul>
-    </article>
+        </div>
     `;
 
     const template = document.createElement('template');
