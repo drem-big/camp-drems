cdd5ae63ef0cdae8803e7bc98a00d09575028421
Cara Ponzini <ponzini@gmail.com>
2019-03-19 14:22:32 -0700

    spiking detail page, moving onto firebase

--- src/campsite-detail.js
+++ src/campsite-detail.js
@@ -7,7 +7,7 @@ const user = {
     photoURL: './assets/alien.png'
 };
 loadHeader(user);
-loadDetail(data);
+// loadDetail(data);
 
 const url = 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/251914/?apikey=cb99ea00-0bd2-4742-bd89-341cf682661d';
 
@@ -18,7 +18,7 @@ fetch(url)
     .then(res => res.json())
     .then(results => {
         console.log(results);
-        // loadDetail(results);
+        loadDetail(results);
     });
 
 function loadDetail(data) {
