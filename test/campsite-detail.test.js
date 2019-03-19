90b5b5ff34c82b94bed656ef7aec3e30d3593afb
miniponz <ponzini@gmail.com>
2019-03-19 16:33:27 -0700

    test modified for detail text

--- test/campsite-detail.test.js
+++ test/campsite-detail.test.js
@@ -16,25 +16,8 @@ test('create html template for campsite detail', assert => {
     const expected = /*html*/`
     <article>
         <h1 id="facility-name">MCNARY BEACH - SOCKEYE SHELTER (OR)</h1>
-        <h3 id="location">Umatilla, OR </h3>
-        <div>
-        <img src="https://cdn.recreation.gov/public/images/76416.jpg" alt="campsite photo">
-        <img src="https://cdn.recreation.gov/public/images/76418.jpg" alt="campsite photo">
-        <img src="https://cdn.recreation.gov/public/images/76436.jpg" alt="campsite photo">
-        <img src="https://cdn.recreation.gov/public/images/76410.jpg" alt="campsite photo">
-        <img src="https://cdn.recreation.gov/public/images/76446.jpg" alt="campsite photo">
-        </div>
+        <p id="location">From Highway 730 turn north onto Beach Access Road, drive for about a mile and then turn right onto McNary Beach Road and that will take you right into McNary Beach Park.</p>
         <div><h2>Overview</h2>McNary Beach is located on Lake Wallula just upstream of McNary Lock and Dam in eastern Oregon on the Mid-Columbia River.<h2>Recreation</h2>The lake provides great boating opportunities with the Oregon Boat Ramp just downstream and a nice designated swimming area.  The trailhead for the Lewis and Clark trail is also located at the east end of the park.<h2>Facilities</h2>McNary Beach offers one day-use group picnic shelter with six tables and two fire grills.<h2>Natural Features</h2>The park is situated along the bank of the Columbia River.  Day users enjoy the relaxing shade of the mature trees throughout the park.<h2>Nearby Attractions</h2>McNary Dam is just downstream with the Pacific Salmon Visitor Center, Oregon Fish Viewing Room and the McNary Dam Wildlife Area with its beautiful natural trails.</div>
-        <h2>Activities</h2>
-        <ul id="activities-list">
-            <li>Hiking</li>
-            <li>Canoeing</li>
-            <li>Day Use Area</li>
-            <li>Fishing</li>
-            <li>Horseback Riding</li>
-            <li>Water Access</li>
-            <li>Swimming</li>
-        </ul>
     </article>
     `;
     //act
@@ -42,6 +25,4 @@ test('create html template for campsite detail', assert => {
 
     //assert
     assert.htmlEqual(result, expected);
-
-
 });
