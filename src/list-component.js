ca8d0bfd2f63bda8100502cc234747c2002bdb56
Cara Ponzini <ponzini@gmail.com>
2019-03-20 10:17:17 -0700

    favorites favoriting

--- src/list-component.js
+++ src/list-component.js
@@ -1,3 +1,5 @@
+import { auth, favoritesByUserRef } from './firebase.js';
+
 export function makeCardTemplate(campsite) {
     let campsiteURL = null;
     if(!campsite.MEDIA[0]) {
@@ -15,6 +17,7 @@ export function makeCardTemplate(campsite) {
     
     const html = /*html*/ `
     <li>
+        <span><img class="campsite-favorites" src="../assets/tentwhite.png"></span>
         <a href="./campsite-detail.html?facilityId=${campsite.FacilityID}" alt="campsite detail page">
             <h3>${campsite.FacilityName}</h3>
             <img src="${campsiteURL}" alt="title.name" class="campsite-image">
@@ -33,6 +36,52 @@ export default function loadCard(campsiteList) {
     clearRows();
     campsiteList.forEach(campsite => {
         const dom = makeCardTemplate(campsite);
+        const campsiteFavorite = dom.querySelector('.campsite-favorites');
+        const userId = auth.currentUser.uid;
+        const userFavoritesRef = favoritesByUserRef.child(userId);
+        const userFavoriteCampsiteRef = userFavoritesRef.child(campsite.FacilityID);
+        userFavoriteCampsiteRef.once('value')
+            .then(snapshot => {
+                const value = snapshot.val();
+                let isFavorite = false;
+                if(value) {
+                    addFavorite();
+                }
+                else {
+                    removeFavorite();
+                }
+
+                function addFavorite(){
+                    isFavorite = true;
+                    campsiteFavorite.src = '../assets/tentcolor.png';
+                    // campsiteFavorite.classList.add('favorite');
+                }
+
+                function removeFavorite() {
+                    isFavorite = false;
+                    campsiteFavorite.src = '../assets/tentwhite.png';
+                }
+
+                campsiteFavorite.addEventListener('click', () => {
+                    if(isFavorite) {
+                        userFavoriteCampsiteRef.remove();
+                        removeFavorite();
+                    }
+                    else {
+                        userFavoriteCampsiteRef.set({
+                            FacilityName: campsite.FacilityName,
+                            FacilityID: campsite.FacilityID,
+                            campsiteURL: campsite.MEDIA[0].URL || 'http://noodleblvd.com/wp-content/uploads/2016/10/No-Image-Available.jpg',
+                            FacilityLatitude: campsite.FacilityLatitude,
+                            FacilityLongitude: campsite.FacilityLongitude,
+                            AddressStateCode: campsite.FACILITYADDRESS[0].AddressStateCode,
+                            City: campsite.FACILITYADDRESS[0].City,
+                            RecAreaName: campsite.RECAREA[0].RecAreaName
+                        });
+                        addFavorite();
+                    }
+                });
+            });
         resultsList.appendChild(dom);
     });
 }
