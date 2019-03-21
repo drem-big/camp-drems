57908ed86e5e33152f8f093e372efca2c41948c6
dreamer <wildetangeant@gmail.com>
2019-03-20 18:05:06 -0700

    latlong works: next makekmakeurl work

--- src/campsite-detail.js
+++ src/campsite-detail.js
@@ -11,9 +11,19 @@ const lon = searchParams.get('lon');
 const facilityUrl = makeFacilityUrl(facilityID);
 const mediaUrl = makeMediaUrl(facilityID);
 const weatherUrl = makeWeatherUrl(lat, lon);
-const googleurl = 'https://maps.googleapis.com/maps/api/staticmap?center=Black%20Rock%20City&zoom=12&size=600x300&maptype=roadmap%20&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318%20&markers=color:red%7Clabel:C%7C40.718217,-73.998284%20&key=AIzaSyC5yJb3hg67IT1ooaA091M9-YUUbF_-svw';
 
+const googleurl = 'https://maps.googleapis.com/maps/api/staticmap?center=Black%20Rock%20City&zoom=12&size=600x300&maptype=roadmap%20&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318%20&markers=color:red%7Clabel:C%7C40.718217,-73.998284%20&key=AIzaSyC5yJb3hg67IT1ooaA091M9-YUUbF_-svw';
+const mapURL = makeMap
 //filter for the thing I want
+const latLon = lat + ',' + lon;
+console.log('latlon is ' + latLon);
+
+export function makeMap(latLon) {
+    const path = `https://maps.googleapis.com/maps/api/staticmap?center=${latLon}&zoom=12&size=600x300&maptype=roadmap%20&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318%20&markers=color:red%7Clabel:C%7C40.718217,-73.998284%20&key=AIzaSyC5yJb3hg67IT1ooaA091M9-YUUbF_-svw`
+
+    const url = new URL(path);
+    return url.toString();
+}
 
 fetch(googleurl)
     // .then(res => res.json())
