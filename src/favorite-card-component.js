b751f7cad81a5fff607ecf71b6e122d2c7d2ba3c
Anna Ridenour <annamridenour@gmail.com>
2019-03-22 09:56:10 -0700

    styling tweaks

--- src/favorite-card-component.js
+++ src/favorite-card-component.js
@@ -1,11 +1,23 @@
 export function makeFavoriteTemplate(campsite) {
+    let campsiteLocation = null;
+    if(campsite.RecAreaName) {
+        campsiteLocation = campsite.RecAreaName + ' , ' + campsite.AddressStateCode;
+    }
+    else if(campsite.City) {
+        campsiteLocation = campsite.City + ' , ' + campsite.AddressStateCode;
+    }
+    else {
+        campsiteLocation = campsite.AddressStateCode;
+    }
+    
+    
     const html = /*html*/ `
         <li>
             <span><img class="campsite-favorites" src="../assets/tentwhite.png"></span>
             <a href="./campsite-detail.html?facilityId=${campsite.FacilityID}&lat=${campsite.FacilityLatitude}&lon=${campsite.FacilityLongitude}" alt="campsite detail page">
                 <h3>${campsite.FacilityName}</h3>
                 <img src="${campsite.campsiteURL}" alt="campsite image" class="campsite-image">
-                <p>Location, ${campsite.AddressStateCode}</p>
+                <p> ${campsiteLocation}</p>
             </a>
         </li>
     `;
